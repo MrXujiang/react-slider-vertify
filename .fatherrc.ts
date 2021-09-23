373e3fe098b688d31df041902b460c7c003b7479
xujiang <xujiang156@qq.com>
2021-09-23 13:47:09 +0800

    :new:添加umd导出

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,7 @@
 export default {
   esm: 'babel',
   cjs: 'babel',
+  umd: {},
   extractCSS: true,
   lessInBabelMode: true,
   runtimeHelpers: true,
--- es/index.js
+++
@@ -1,1 +0,0 @@
-export { default as Vertify } from './Vertify';
